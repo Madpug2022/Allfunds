11892e95ea65d15e9ddce93e6e50c3525464d5f9
Madpug2022 <matias.chiappa@hotmail.com>
2024-06-16 20:32:20 +0200

    [AF-2] Creacion de cliente de peticiones de api

--- src/app/(generalLayout)/loading.tsx
+++ src/app/(generalLayout)/loading.tsx
@@ -1,8 +1,9 @@
+import Loader from '@/components/ui/Loader/Loader'
 import React from 'react'
 
 const loading = () => {
     return (
-        <div>loading...</div>
+        <Loader />
     )
 }
 
---
+++ src/clientApi/groceriesApi/index.ts
@@ -0,0 +1,30 @@
+import { requests } from ".."
+
+const GROCERIES_API = {
+    getGroceries: async () => {
+        try {
+            const response = await requests.get("/grocery");
+            return response;
+        } catch (error) {
+            throw error
+        }
+    },
+    updateStock: async (groceryId: string, stock: number) => {
+        try {
+            const response = await requests.patch(`/grocery/${groceryId}`, { stock });
+            return response;
+        } catch (error) {
+            throw error
+        }
+    },
+    getFavorite: async (listId: string) => {
+        try {
+            const response = await requests.get(`/grocery?favorite=${listId}`);
+            return response;
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+export default GROCERIES_API;
